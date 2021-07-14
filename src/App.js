14eee65162c3a95bb36943074157d07e6f33935c
nlom0218 <nlom0218@naver.com>
2021-07-14 18:55:10 +0900

    create Home page

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
 import React from 'react';
-import styled from 'styled-components';
 import Header from './components/Header';
 import { GlobalStyle } from './styles';
-
-const Main = styled.div`
-  height: 100%;
-  width: 1200px;
-  margin: 0 auto;
-`
-
-const Container = styled.div`
-  display: grid;
-  grid-template-columns: repeat(12, 1fr);
-  grid-template-rows: repeat(5, 30px);
-`
-
-const Hello = styled.div`
-  background-color: blue;
-  grid-column: 1 / 11;
-  grid-row: 1 / 4;
-`
-
-const Hi = styled.div`
-  grid-column: 11 / 13 ;
-  background-color: red;
-`
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Home from './pages/Home';
 
 function App() {
   return (
     <React.Fragment>
       <GlobalStyle />
       <Header />
-      {/* <Main>
-        <Container>
-          <Hello>Hello</Hello>
-          <Hi>Hi</Hi>
-        </Container>
-      </Main> */}
+      <Router>
+        <Switch>
+          <Route exact path="/"><Home /></Route>
+        </Switch>
+      </Router>
     </React.Fragment>
   );
 }
