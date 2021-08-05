4d9c8998f21c9f1d671d157ea1c2910271e3f24a
nlom0218 <nlom0218@naver.com>
2021-08-05 17:20:17 +0900

    create eidt page

---
+++ src/components/Edit/EditQuestion.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div``
+
+const EditQuestion = () => {
+  return (<Container>edit question</Container>);
+}
+
+export default EditQuestion;
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useLocation, useParams } from 'react-router';
 import BasicContainer from '../components/BasicContainer';
+import EditQuestion from '../components/Edit/EditQuestion';
+import EditQuiz from '../components/Edit/EditQuiz';
 import Header from '../components/Header';
 import NavBtn from '../components/NavBtn';
 
 const Edit = () => {
+  const { type } = useParams()
+  const location = useLocation()
+  console.log(location);
   return (<React.Fragment>
     <Header />
     <BasicContainer>
-
+      {type === "quiz" && <EditQuiz />}
+      {type === "question" && <EditQuestion />}
     </BasicContainer>
     <NavBtn />
   </React.Fragment>);
