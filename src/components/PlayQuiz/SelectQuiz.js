0aaf6a6285a186eb30d616df8ef7a38522090da8
nlom0218 <nlom0218@naver.com>
2021-08-12 21:35:36 +0900

    SelectQuiz

--- src/components/PlayQuiz/SelectQuiz.js
+++ src/components/PlayQuiz/SelectQuiz.js
@@ -1,28 +1,40 @@
 import { useQuery } from '@apollo/client';
-import { faBook } from '@fortawesome/free-solid-svg-icons';
+import { faBook, faHandPointer } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import gql from 'graphql-tag';
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory, useParams } from 'react-router';
 import styled from 'styled-components';
 import { fadeIn } from '../../animation/fade';
 
-const Container = styled.div``
+const Container = styled.div`
+  display: grid;
+  row-gap: 30px;
+`
 
-const QuizTitle = styled.div`
-  line-height: 24px;
-  .quizTitle-column {
-    display: grid;
-    row-gap: 20px;
-    animation: ${fadeIn} 0.6s ease;
-  }
-  .column-title {
-    justify-self: flex-start;
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  column-gap: 30px;
+  .leftContent {
+    align-self: flex-start;
     background-color: rgb(255, 165, 0, 0.4);
     padding: 5px 10px;
     border-radius: 5px;
   }
+  .rightContent {
+    align-self: center;
+  }
 `
+
+const SelectBtn = styled.div`
+  cursor: pointer;
+  transition: background-color linear 0.5s;
+  :hover {
+    background-color: rgb(255, 165, 0, 0.8);
+  }
+`
+
 
 const DETATIL_QUIZ_QUERY = gql`
   query detailQuiz($id: Int!) {
@@ -36,22 +48,24 @@ const DETATIL_QUIZ_QUERY = gql`
 const SelectQuiz = () => {
   const { id } = useParams()
   const history = useHistory()
-  console.log(id);
+  const [seeList, setSeeList] = useState(false)
   const { data, loading } = useQuery(DETATIL_QUIZ_QUERY, {
     variables: { id: parseInt(id) },
     skip: !id
   })
+  const onClickSeletBtn = () => {
+    setSeeList((prev) => !prev)
+  }
   return (<Container>
-    <QuizTitle>{id ?
-      <div className="quizTitle-column">
-        <div className="column-title"><FontAwesomeIcon icon={faBook} /> 선택된 퀴즈</div>
-        <div className="column-content">{data?.detailQuiz?.title}</div>
-      </div>
-      : "선택된 퀴즈가 없습니다."}
-    </QuizTitle>
-    <div onClick={() => history.push("/play-quiz/39")}>39</div>
-    <div onClick={() => history.push("/play-quiz/40")}>40</div>
-  </Container>);
+    <Wrapper>
+      <div className="leftContent"><FontAwesomeIcon icon={faBook} /> 선택된 퀴즈</div>
+      <div className="rightContent">{id ? data?.detailQuiz?.title : "선택된 퀴즈가 없습니다."}</div>
+    </Wrapper>
+    <Wrapper>
+      <SelectBtn className="leftContent" onClick={onClickSeletBtn}><FontAwesomeIcon icon={faHandPointer} /> 퀴즈 선택하기</SelectBtn>
+    </Wrapper>
+
+  </Container >);
 }
 
 export default SelectQuiz;
