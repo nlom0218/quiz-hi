95246ecba086db1219bfbbac5b51d95ba4ef8d31
nlom0218 <nlom0218@naver.com>
2021-09-20 13:44:18 +0900

    Home NavIcon explan page

--- src/components/Home/NavIcon.js
+++ src/components/Home/NavIcon.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBookReader, faMoon, faPencilAlt, faPlay, faSearch, faSignInAlt, faSignOutAlt, faSun, faUser } from '@fortawesome/free-solid-svg-icons';
-import { faClipboard, faListAlt, faQuestionCircle } from '@fortawesome/free-regular-svg-icons';
+import { faClipboard, faListAlt, faNewspaper, faQuestionCircle } from '@fortawesome/free-regular-svg-icons';
 import { useHistory } from 'react-router';
 import { darkModeVar, disableDarkMode, enableDarkMode } from '../../apollo';
 import { useReactiveVar } from '@apollo/client';
@@ -23,6 +23,20 @@ const Wapper = styled.div`
   grid-template-rows: 1fr 2fr;
   justify-items: center;
   transition: background-color 0.3s linear;
+  cursor: pointer;
+  :hover {
+    background-color: rgb(140, 255, 237, 0.6);
+  }
+`
+
+const BlogLink = styled.a`
+  display: grid;
+  grid-template-rows: 1fr 2fr;
+  justify-items: center;
+  transition: background-color 0.3s linear;
+  div {
+    transition: color 1s ease;
+  }
   cursor: pointer;
   :hover {
     background-color: rgb(140, 255, 237, 0.6);
@@ -78,12 +92,14 @@ const NavIcon = () => {
         </Icon>
         <Description>다크모드 & 라이트모드</Description>
       </Wapper>
-      <Wapper>
+      <BlogLink
+        href="https://khd0218.tistory.com/pages/%ED%80%B4%EC%A6%88-%ED%95%98%EC%9D%B4-%EC%84%A4%EB%AA%85%EC%84%9C"
+        target="_blank">
         <Icon>
-          <FontAwesomeIcon icon={faSearch} />
+          <FontAwesomeIcon icon={faNewspaper} />
         </Icon>
-        <Description>퀴즈 검색하기</Description>
-      </Wapper>
+        <Description>설명서 바로가기</Description>
+      </BlogLink>
       <Wapper onClick={() => onClinkNavBtn("feed/quiz/all/recent/1")}>
         <Icon>
           <FontAwesomeIcon icon={faClipboard} />
