1b3f8a9637b30db91b5c52558f372026dec76bd9
nlom0218 <nlom0218@naver.com>
2021-09-21 14:48:23 +0900

    sharedFn score process

--- src/sharedFn.js
+++ src/sharedFn.js
@@ -39,26 +39,28 @@ export const processUserLevel = (type, score) => {
       return 10
     }
   }
-  if (score < 20) {
-    return 1
-  } else if (score < 60) {
-    return 2
-  } else if (score < 140) {
-    return 3
-  } else if (score < 300) {
-    return 4
-  } else if (score < 620) {
-    return 5
-  } else if (score < 1240) {
-    return 6
-  } else if (score < 2520) {
-    return 7
-  } else if (score < 5080) {
-    return 8
-  } else if (score < 10200) {
-    return 9
-  } else {
-    return 10
+  if (type === "teacher") {
+    if (score < 20) {
+      return 1
+    } else if (score < 60) {
+      return 2
+    } else if (score < 140) {
+      return 3
+    } else if (score < 300) {
+      return 4
+    } else if (score < 620) {
+      return 5
+    } else if (score < 1240) {
+      return 6
+    } else if (score < 2520) {
+      return 7
+    } else if (score < 5080) {
+      return 8
+    } else if (score < 10200) {
+      return 9
+    } else {
+      return 10
+    }
   }
 }
 
@@ -84,24 +86,26 @@ export const processNextLevelScore = (type, level, score) => {
       return 10000 - score
     }
   }
-  if (level === 1) {
-    return 20 - score
-  } else if (level === 2) {
-    return 60 - score
-  } else if (level === 3) {
-    return 140 - score
-  } else if (level === 4) {
-    return 300 - score
-  } else if (level === 5) {
-    return 620 - score
-  } else if (level === 6) {
-    return 1240 - score
-  } else if (level === 7) {
-    return 2520 - score
-  } else if (level === 8) {
-    return 5080 - score
-  } else if (level === 9) {
-    return 10200 - score
+  if (type === "teacher") {
+    if (level === 1) {
+      return 20 - score
+    } else if (level === 2) {
+      return 60 - score
+    } else if (level === 3) {
+      return 140 - score
+    } else if (level === 4) {
+      return 300 - score
+    } else if (level === 5) {
+      return 620 - score
+    } else if (level === 6) {
+      return 1240 - score
+    } else if (level === 7) {
+      return 2520 - score
+    } else if (level === 8) {
+      return 5080 - score
+    } else if (level === 9) {
+      return 10200 - score
+    }
   }
 }
 
