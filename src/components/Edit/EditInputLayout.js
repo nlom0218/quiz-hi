a4694adb661e581a59335643e4cf689c0423bdad
nlom0218 <nlom0218@naver.com>
2021-09-24 11:57:51 +0900

    EditInputLayout bgColor

--- src/components/Edit/EditInputLayout.js
+++ src/components/Edit/EditInputLayout.js
@@ -7,7 +7,7 @@ const Wrapper = styled.div`
   input {
     padding: 10px 20px;
     border-radius: 5px;
-    background-color: rgb(200, 200, 200, 0.2);
+    /* background-color: rgb(200, 200, 200, 0.2); */
     transition: box-shadow 0.4s linear;
     :focus {
       box-shadow: 0 0 1px 0.5px ${props => props.theme.fontColor};
